17b2db597e4621d0ee5ee38c0b91ba2d592b3050
1mm <yyg1223@gmail.com>
2016-08-24 19:57:23 +0900

    1mm dummy

--- routes/question.js
+++ routes/question.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var isSecure = require('./common').isSecure;
 
-router.get('/', function (req, res, next) {
+router.get('/', isSecure, function (req, res, next) {
 
     // 6. 팔로우 질문 리스트
     if (req.url.match(/\/\?direction=to&celebrity=true&pageNo=\d+&count=\d+/i)) {
@@ -20,12 +21,18 @@ router.get('/', function (req, res, next) {
             }]
         });
     }
-    
 
-    // 11. 나의 질문 목록 to (내가 질문한 목록)
-    if (req.url.match(/\/\?direction=to&pageNo=\d+&count=\d+/i)) {
+    // 11. 질문한 목록 to
+    if (req.url.match(/\/\?direction=to&id=.*&pageNo=\d+&count=\d+/i)) {
+        var id;
+        if (req.query.id === 'me') {
+            id = 'me';
+        } else {
+            id = parseInt(req.query.id, 10);
+        }
+        console.log(id);
         res.send({
-            message: "11. 나의 질문 목록 to (내가 질문한 목록)",
+            message: "질문한 목록 to",
             result: [{
                 Q_id: "1",
                 A_id: "2",
@@ -39,46 +46,18 @@ router.get('/', function (req, res, next) {
             }]
         });
     }
-
-    // 11. 나의 질문 목록 from (내가 받은 질문 목록)
-    if (req.url.match(/\/\?direction=from&pageNo=\d+&count=\d+/i)) {
+    // 11. 질문받은 목록 from
+    if (req.url.match(/\/\?direction=from&id=.*&pageNo=\d+&count=\d+/i)) {
+        var id;
+        if (req.query.id === 'me') {
+            id = 'me';
+        } else {
+            id = parseInt(req.query.id, 10);
+        }
+        if (req.query.id === 'me') {
+        }
         res.send({
-            message: "11. 나의 질문 목록 from (내가 받은 질문 목록)",
-            result: [{
-                Q_id: "1",
-                A_id: "2",
-                Q_photo: "http://localhost/images/123.jpg",
-                Q_content: "질문내용",
-                price: "10000",
-                A_photo: "http://localhost/images/456.jpg",
-                voice_content: "http://localhost/voice/20160821.mp3",
-                listen_count: "103040",
-                length: "35"
-            }]
-        });
-    }
-
-    // 12. 상대방 질문 목록 to (상대방이 질문한 목록)
-    if (req.url.match(/\/\?direction=to&id=\d+&pageNo=\d+&count=\d+/i)) {
-        res.send({
-            message: "12. 상대방 질문 목록 to (상대방이 질문한 목록)",
-            result: [{
-                Q_id: "1",
-                A_id: "2",
-                Q_photo: "http://localhost/images/123.jpg",
-                Q_content: "질문내용",
-                price: "10000",
-                A_photo: "http://localhost/images/456.jpg",
-                voice_content: "http://localhost/voice/20160821.mp3",
-                listen_count: "103040",
-                length: "35"
-            }]
-        });
-    }
-    // 12. 상대방 질문 목록 to (상대방이 질문 받은 목록)
-    if (req.url.match(/\/\?direction=from&id=\d+&pageNo=\d+&count=\d+/i)) {
-        res.send({
-            message: "12. 상대방 질문 목록 from (상대방이 질문 받은 목록)",
+            message: " 질문받은 목록 from",
             result: [{
                 Q_id: "1",
                 A_id: "2",
@@ -95,8 +74,7 @@ router.get('/', function (req, res, next) {
 });
 
 // 13. 인기 질문 리스트
-router.get('/popular10', function (req, res, next) {
-
+router.get('/popular10', isSecure, function (req, res, next) {
     res.send({
         message: "13. 인기 질문 리스트",
         result: [{
@@ -111,7 +89,6 @@ router.get('/popular10', function (req, res, next) {
             length: "35"
         }]
     });
-
 });
 
 
@@ -125,7 +102,7 @@ router.post('/', function (req, res, next) {
 
     res.send({
         message: "질문이 등록 되었습니다."
-    })
+    });
 });
 
 
